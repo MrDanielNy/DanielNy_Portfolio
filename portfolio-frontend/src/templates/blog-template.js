425a5a04b7713c6c86f67c2dff2aeb87502b0a6b
Daniel Ny <tjanster1@danielny.se>
2020-10-16 14:56:57 +0200

    Fixed blog template

--- portfolio-frontend/src/templates/blog-template.js
+++ portfolio-frontend/src/templates/blog-template.js
@@ -3,24 +3,26 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import ReactMarkdown from "react-markdown"
 import SEO from "../components/SEO"
+import { BlogWrapper } from '../elements/BlogTemplateElements'
 
 const ComponentName = ({ data }) => {
   console.log(data);
   const { content, title, desc } = data.blog
-
+ 
   return (
     <Layout>
       <SEO title={title} description={desc} />
-      <section className="blog-template">
-        <div className="section-center">
-          <article className="blog-content">
+      <BlogWrapper>
+       {/*} <div className="section-center">*/}
+          <article>
             <ReactMarkdown source={content} />
           </article>
-          <Link to="/blog" className="btn center-btn">
+          <Link to="/blog">
             All articles
           </Link>
-        </div>
-      </section>
+       {/* </div> */}
+       
+      </BlogWrapper>
     </Layout>
   )
 }
